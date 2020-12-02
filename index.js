87989d54127d1fc6d131c462e6878886685f89be
react native doctor <yekinnijibola@gmail.com>
2020-12-03 00:55:34 +0100

    added event listeners to icons

---
+++ index.js
@@ -0,0 +1,26 @@
+// element dom references
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+
+//array of video names
+
+videos = ["", "", "", "", ""];
+
+one.addEventListener("mouseover", () => {
+  alert("one");
+});
+two.addEventListener("mouseover", () => {
+  alert("two");
+});
+three.addEventListener("mouseover", () => {
+  alert("three");
+});
+four.addEventListener("mouseover", () => {
+  alert("four");
+});
+five.addEventListener("mouseover", () => {
+  alert("five");
+});
